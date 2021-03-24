15acc30e0fd16951d55698424646db37eec989b7
Oscar Pari <oscarjhin@gmail.com>
2021-03-23 21:04:16 -0400

    version 2

--- src/java/com/emergentes/LoginSession.java
+++ src/java/com/emergentes/LoginSession.java
@@ -13,24 +13,6 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "LoginSession", urlPatterns = {"/LoginSession"})
 public class LoginSession extends HttpServlet {
 
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginSession</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginSession at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
